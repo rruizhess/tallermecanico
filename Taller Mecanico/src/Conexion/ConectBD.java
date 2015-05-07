4bad30f940452323b69e66378e3b6ae659074850
Cristian Evens <cristian.evens@outlook.com>
2015-05-06 23:14:15 -0400

    creacion login y acceso a bd

---
+++ Taller Mecanico/src/Conexion/ConectBD.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Conexion;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Cristian
+ */
+public class ConectBD {
+    
+  private String bd =   "";    //<-----nombre Base Datos
+   private String login = "root";//<---- user base de datos
+   private String password = "jidokwan";//<----pass bd
+   private String url = "jdbc:mysql://localhost:3306/"+bd;// direccion bd 
+
+  public ConectBD(String user, String pass){
+    this.login=user;
+    this.password=pass;
+    
+    }
+
+   
+   public Connection Conectar(){
+
+       Connection link = null;
+
+       try{
+
+           Class.forName("org.gjt.mm.mysql.Driver");
+
+           link = DriverManager.getConnection(this.url, this.login, this.password);
+           
+
+       }catch(Exception ex){
+
+           JOptionPane.showMessageDialog(null, "No Esta Logeado");
+          
+
+       }
+
+
+       return link;
+
+   }
+   
+   
+}
--- Taller Mecanico/src/Login.java
+++ Taller Mecanico/src/Login.java
@@ -1,3 +1,8 @@
+
+import Conexion.ConectBD;
+import static java.awt.image.ImageObserver.WIDTH;
+import javax.swing.JOptionPane;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -27,24 +32,105 @@ public class Login extends javax.swing.JInternalFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        b_user = new javax.swing.JTextField();
+        b_log = new javax.swing.JButton();
+        p_pass = new javax.swing.JPasswordField();
+
         setClosable(true);
         setIconifiable(true);
+
+        jLabel1.setText("Usuario        :");
+
+        jLabel2.setFont(new java.awt.Font("Castellar", 1, 14)); // NOI18N
+        jLabel2.setForeground(new java.awt.Color(51, 51, 51));
+        jLabel2.setText("INGRESO ");
+
+        jLabel3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/mecanicologin.jpg"))); // NOI18N
+
+        jLabel4.setText("Contraseña :");
+
+        b_log.setText("Log-in");
+        b_log.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                b_logActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 378, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(jLabel3)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel2)
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                        .addGroup(layout.createSequentialGroup()
+                            .addComponent(jLabel4)
+                            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                            .addComponent(p_pass))
+                        .addGroup(layout.createSequentialGroup()
+                            .addComponent(jLabel1)
+                            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                            .addComponent(b_user, javax.swing.GroupLayout.PREFERRED_SIZE, 73, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGroup(layout.createSequentialGroup()
+                            .addGap(31, 31, 31)
+                            .addComponent(b_log))))
+                .addContainerGap(65, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 279, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(33, 33, 33)
+                .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 31, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jLabel3, javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel1)
+                            .addComponent(b_user, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel4)
+                            .addComponent(p_pass, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(28, 28, 28)
+                        .addComponent(b_log)))
+                .addContainerGap(38, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void b_logActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_b_logActionPerformed
+        // TODO add your handling code here:
+        String user=b_user.getText();
+        String pass=String.valueOf(p_pass.getPassword());
+        if("user".equals(user) && "pass".equals(pass)){
+       ConectBD c=new ConectBD(user,pass);
+       c.Conectar();
+       this.dispose();
+       
+        }
+
+        else{
+            JOptionPane.showMessageDialog(rootPane, "Login Incorrecto", title, WIDTH, frameIcon);
+        }
+    }//GEN-LAST:event_b_logActionPerformed
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton b_log;
+    private javax.swing.JTextField b_user;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JPasswordField p_pass;
     // End of variables declaration//GEN-END:variables
 }
